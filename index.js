69d76a1fd88023c2cca235a545ae3f49fce98892
ForbesLindesay <forbes@lindesay.co.uk>
2015-08-07 15:04:10 +0100

    Mark <textarea> as whitespace sensitive
    [closes jadejs/jade#1931]

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ var constantinople = require('constantinople');
 var stringify = require('js-stringify');
 var addWith = require('with');
 
+// This is used to prevent pretty printing inside certain tags
+var WHITE_SPACE_SENSITIVE_TAGS = {
+  pre: true,
+  textarea: true
+};
+
 var INTERNAL_VARIABLES = [
   'jade',
   'jade_mixins',
@@ -501,7 +507,7 @@ Compiler.prototype = {
       else self.buffer(name);
     }
 
-    if ('pre' == tag.name) this.escape = true;
+    if (WHITE_SPACE_SENSITIVE_TAGS[tag.name] === true) this.escape = true;
 
     if (!this.hasCompiledTag) {
       if (!this.hasCompiledDoctype && 'html' == name) {
@@ -538,7 +544,7 @@ Compiler.prototype = {
       this.visit(tag.block);
 
       // pretty print
-      if (pp && !tag.isInline && 'pre' != tag.name && !tagCanInline(tag))
+      if (pp && !tag.isInline && WHITE_SPACE_SENSITIVE_TAGS[tag.name] !== true && !tagCanInline(tag))
         this.prettyIndent(0, true);
 
       this.buffer('</');
@@ -546,7 +552,7 @@ Compiler.prototype = {
       this.buffer('>');
     }
 
-    if ('pre' == tag.name) this.escape = false;
+    if (WHITE_SPACE_SENSITIVE_TAGS[tag.name] === true) this.escape = false;
 
     this.indents--;
   },

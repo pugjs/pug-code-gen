12551c3b2fc9d3cee3d5b323887647ecdac40f99
Timothy Gu <timothygu99@gmail.com>
2015-09-23 20:48:30 -0700

    Remove handling of interpolation
    For interpolation in text ("p Hey #{myVar}"), the separation of
    interpolated text from normal text is now handled in jade-lexer
    (with support in jade-parser).
    
    For interpolation in attributes, it is removed. See #2095.

--- index.js
+++ index.js
@@ -173,26 +173,8 @@ Compiler.prototype = {
    * @api public
    */
 
-  buffer: function (str, interpolate) {
+  buffer: function (str) {
     var self = this;
-    if (interpolate) {
-      var match = /(\\)?([#!]){((?:.|\n)*)$/.exec(str);
-      if (match) {
-        this.buffer(str.substr(0, match.index), false);
-        if (match[1]) { // escape
-          this.buffer(match[2] + '{', false);
-          this.buffer(match[3], true);
-          return;
-        } else {
-          var rest = match[3];
-          var range = parseJSExpression(rest);
-          var code = ('!' == match[2] ? '' : this.runtime('escape')) + "((jade_interp = " + range.src + ") == null ? '' : jade_interp)";
-          this.bufferExpression(code);
-          this.buffer(rest.substr(range.end + 1), true);
-          return;
-        }
-      }
-    }
 
     str = stringify(str);
     str = str.substr(1, str.length - 2);
@@ -224,7 +206,7 @@ Compiler.prototype = {
 
   bufferExpression: function (src) {
     if (isConstant(src)) {
-      return this.buffer(toConstant(src) + '', false)
+      return this.buffer(toConstant(src) + '')
     }
     if (this.lastBufferedIdx == this.buf.length && this.bufferedConcatenationCount < 100) {
       this.bufferedConcatenationCount++;
@@ -608,7 +590,7 @@ Compiler.prototype = {
    */
 
   visitText: function(text){
-    this.buffer(text.val, true);
+    this.buffer(text.val);
   },
 
   /**
